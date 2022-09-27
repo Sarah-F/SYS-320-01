3d13a0a1f6b33f30e928778b9fc8fb2695b4bac1
Sarah-F <60114072+Sarah-F@users.noreply.github.com>
2022-09-26 23:23:14 -0400

    Add files via upload

--- Week4/Homework/searchLog.py
+++ Week4/Homework/searchLog.py
@@ -2,25 +2,26 @@ import re, sys, yaml
 import searchLog
 
 # Sifts through logs
-def logs (logFile,searchTerm):
+def logs(logFile,searchTerm):
     # Opens searchTerms.yaml
         try:
             with open('searchTerms.yaml', 'r') as yf:
                 keywords = yaml.safe_load_all(yf)
 
                 # Creates list for the list of keywords to scan
-                listOfKeywords = []
-                for eachEntry in keywords:
-                    for key,value in eachEntry[searchTerm].items():
-                        listOfKeywords.append(value)
+                # #Query the ymal for the terms specified inside
+                terms = searchTerm
                 
+                #Split the etries by the commas
+                listOfKeywords = terms.split(", ")
+               
                 # Opens log file 
                 with open(logFile) as f:
 
                     # Reads file and saves it to a variable
                     contents = f.readlines()
 
-                 # Creates list to store results
+                # Creates list to store results
                 results = []
 
                 # Loops through contents list
@@ -29,9 +30,9 @@ def logs (logFile,searchTerm):
                     # Loops through  keywords
                     for eachKeyword in listOfKeywords:
 
-                        # Searches and returns results
-                        x = re.findall("%s" %eachKeyword, line)
-
+                        # If the 'line' contains the keyword then it will print
+                        # Searches returned results using a regular expression
+                        x = re.findall(r''+eachKeyword+'', line)
 
                         for found in x:
                             # Appends returned keywords to results list
@@ -49,3 +50,7 @@ def logs (logFile,searchTerm):
 
         except EnvironmentError as e:
             print(e.strerror)
+#python3
+#pip3 install pyyaml
+#pip install --upgrade pip
+#pip3 install --upgrade pip
