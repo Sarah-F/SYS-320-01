d9e5f5bdf9b9812437e5ef4b071f570f8f4fc668
Sarah-F <60114072+Sarah-F@users.noreply.github.com>
2022-10-04 18:57:15 -0400

    Add files via upload

--- Week5/Homework/Oldfs.py
+++ Week5/Homework/Oldfs.py
@@ -17,77 +17,31 @@ args = parser.parse_args()
 # Stores root directory
 rootdir = args.directory
 # Stores search term
-searchTerm = args.searchTerm
+searchT = args.searchTerm
 
-# Check if the argument is a directory (to test on Mac)
+# Check if the argument is a directory
 if not os.path.isdir(rootdir):
     print("Invalid directory => {}".format(rootdir))
     exit()
 
-
-
-
-
-# Creates logs function, function sifts through logs
-def logs(filename,searchTerm):
-    # List to save files
-    fList = []
-
-    # Crawl through the provided directory
-    for root, subfolders, filenames in os.walk(rootdir):
-        for f in filenames:
-            #print(root  + "/" + f)
-            fileList = root  + "/" + f
-            #print(fileList)
-            fList.append(fileList)    
-        # Creates a csv reader object, stores as f
-        contents = csv.reader(f)
-        
-        # List to store the results
-        results = []
-
-        # Loops through each line
-        for eachLine in contents:
-            # for loop searched for keywords in searchTerms;
-            for keyword in searchTerm:
-                # Searches for specific keywords
-                x = re.findall(r'' + keyword + '', eachLine[1])
-                
-                for found in x:
-                        # Appends new results to results
-                        results.append(eachLine)
-
-        return results
-
-
-
-
-
-
-
-
-
-
-# List to save files
+# save files to list
 fList = []
 
 # Crawl through the provided directory
 for root, subfolders, filenames in os.walk(rootdir):
     for f in filenames:
-        #print(root  + "/" + f)
         fileList = root  + "/" + f
-        #print(fileList)
-        fList.append(fileList)
-
-# Start looking through each log file in the directory
+        fList.append(fileList)    
+    
+# Looks through each log in the directory
 keywords = []
-# Looks through each log file in directory
 for eachFile in fList:
-    # Loads cvs and stores spliced results
-    results = searchLogCsv.log(eachFile,keywords[1:])
+    
+    # Loads CSV, stores spliced results
+    results = searchLogCsv.logs(eachFile,keywords[1:])    
 
     # Gets keywords from yaml file
-    keywords = searchLogYaml.log(searchTerm)
+    keywords = searchLogYaml.logs(searchT)
 
     # print out results if there was a match found
     if len(results) > 0:
@@ -101,6 +55,10 @@ for eachFile in fList:
                 Path: %s
                 PID: %s
                 Username: %s
-                """.format(searchTerm,eachFile, results[1], keywords[0], results[2],results[3], results[5], results[6]))
+                """.format(searchT,eachFile, results[1], keywords[0], results[2],results[3], results[5], results[6]))
+                # changed serachTerm to searchT
 
-
+#cd ~/Desktop/SYS-320-01/Week5/Homework
+#python3 Oldfs.py -d/Users/sarahfornaldes/Desktop/SYS-320-01/Week5/logs -s/ps1
+#python3 Oldfs.py -d/Users/sarahfornaldes/Desktop/SYS-320-01/Week5/logs -s/Powershell
+#python3 Oldfs.py -d/Users/sarahfornaldes/Desktop/SYS-320-01/Week5/logs -s/Detections
