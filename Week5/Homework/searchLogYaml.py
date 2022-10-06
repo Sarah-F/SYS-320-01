898620c1755522eae708fdd0ab8030d96744de2f
Sarah-F <60114072+Sarah-F@users.noreply.github.com>
2022-10-05 21:43:09 -0400

    Add files via upload

---
+++ Week5/Homework/searchLogYaml.py
@@ -0,0 +1,17 @@
+import csv, re, yaml
+
+# Creates logs function, function sifts through logs
+def logs(term):
+
+    listOfKeywords = []
+    # Open the searchTerms.yaml file
+    try:
+        with open('searchTerms.yaml', 'r') as yf:
+            keywords = yaml.safe_load_all(yf)
+    except EnvironmentError as e:
+        print(e.strerror)
+            
+        #Split the etries by the commas
+        listOfKeywords = term.split(", ")
+
+    return listOfKeywords
